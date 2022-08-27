3351c8b91e466d4ae72de1717629142a5a7bc56b
Felipe <felipe.melo@luizalabs.com>
2022-08-27 20:53:59 -0300

    chore: setup AT tests

--- AT/features/login/environment.py
+++
@@ -1,9 +0,0 @@
-from behave import fixture, use_fixture
-from selenium import webdriver
-
-from features.fixtures import selenium_browser_chrome
-
-
-def before_all(context):
-    use_fixture(selenium_browser_chrome, context)
-
