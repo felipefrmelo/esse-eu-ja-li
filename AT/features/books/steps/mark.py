4cc1c02ef2dd6806062f286cb4e1ff5ab09e8b14
Felipe <felipe.melo@luizalabs.com>
2022-08-28 09:30:02 -0300

    test: add acceptance test to mark book

---
+++ AT/features/books/steps/mark.py
@@ -0,0 +1,32 @@
+from behave import *
+
+@given(u'que eu esteja pagina de listagem de livros')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given que eu esteja pagina de listagem de livros')
+
+
+@when(u'eu clico no icone de marcar')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When eu clico no icone de marcar')
+
+
+@then(u'o icone deve altenar de cor sinalizado que está marcado')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then o icone deve altenar de cor sinalizado que está marcado')
+
+
+@given(u'que esteja na pagina de detalhe do livro')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given que esteja na pagina de detalhe do livro')
+
+
+@when(u'clico no botão "Esse eu já li" com fundo transparente')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When clico no botão "Esse eu já li" com fundo transparente')
+
+
+@then(u'o fundo do botão deve mudar para verde')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then o fundo do botão deve mudar para verde')
+
+
