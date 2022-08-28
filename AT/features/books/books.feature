dc97a8ba78f485b527f5577efb04d97f80472897
Felipe <felipe.melo@luizalabs.com>
2022-08-27 22:37:56 -0300

    test: add acceptance test to list books

---
+++ AT/features/books/books.feature
@@ -0,0 +1,8 @@
+#language: pt
+
+Funcionalidade: Listar livros
+
+    Cenario: Cliente consulta livros cadastrados no site
+
+      Dado que eu esteja na pagina principal
+      Então eu devo viasualizar uma lista de livros
---
+++ AT/features/books/steps/books.py
@@ -0,0 +1,12 @@
+from behave import *
+
+@given(u'que eu esteja na pagina principal')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given que eu esteja na pagina principal')
+
+
+@then(u'eu devo viasualizar uma lista de livros')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then eu devo viasualizar uma lista de livros')
+
+
