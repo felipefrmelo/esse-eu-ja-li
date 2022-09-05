cf5fbe2380a9d837e950faf082dffc9786087476
Felipe <felipe.melo@luizalabs.com>
2022-09-05 20:57:11 -0300

    feat: add service to list books

---
+++ src/backend/src/services/list_books.rs
@@ -0,0 +1,78 @@
+trait BookRepository {
+    fn get_books_by_text(&self, query: &str) -> Vec<Book>;
+}
+
+#[derive(Clone)]
+pub struct Book {
+    id: String,
+    title: String,
+    description: String,
+    image: String,
+}
+
+impl Book {
+    pub fn new(id: String, title: String, description: String, image: String) -> Self {
+        Book {
+            id,
+            title,
+            description,
+            image,
+        }
+    }
+}
+
+fn handle_list_books(query: &str, repository: &impl BookRepository) -> Vec<Book> {
+    repository.get_books_by_text(query)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct FakeBookRepository {
+        books: Vec<Book>,
+    }
+
+    impl FakeBookRepository {
+        pub fn new(books: Vec<Book>) -> Self {
+            Self { books }
+        }
+    }
+
+    impl BookRepository for FakeBookRepository {
+        fn get_books_by_text(&self, _: &str) -> Vec<Book> {
+            self.books.clone()
+        }
+    }
+
+    fn make_books(quantity: u16) -> Vec<Book> {
+        (0..quantity)
+            .map(|i| {
+                Book::new(
+                    format!("id {}", i + 1),
+                    format!("book {}", i + 1),
+                    format!("description {}", i + 1),
+                    format!("image {}", i + 1),
+                )
+            })
+            .collect()
+    }
+
+    #[test]
+    fn should_list_books() {
+        let repo = FakeBookRepository::new(make_books(5));
+        let query = "book";
+
+        let books: Vec<Book> = handle_list_books(query, &repo);
+
+        let book = books.first();
+
+        assert!(book.is_some());
+        let book = book.unwrap();
+
+        assert_eq!(book.id, "id 1".to_string());
+        assert_eq!(book.title, "book 1".to_string());
+        assert_eq!(book.description, "description 1".to_string());
+        assert_eq!(book.image, "image 1".to_string());
+    }
+}
--- src/backend/src/services/mod.rs
+++ src/backend/src/services/mod.rs
@@ -1,1 +1,2 @@
+pub mod list_books;
 pub mod login;
