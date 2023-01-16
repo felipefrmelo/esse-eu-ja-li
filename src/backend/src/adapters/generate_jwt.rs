4923f62122e3f5429f1f01f50e23ba0809f9848f
Felipe <felipe.melo@luizalabs.com>
2023-01-15 21:59:09 -0300

    refactor: change enpoints to use token

--- src/backend/src/adapters/generate_jwt.rs
+++ src/backend/src/adapters/generate_jwt.rs
@@ -1,3 +1,4 @@
+use chrono::{Duration, UTC};
 use jsonwebtoken::{encode as jwt_encode, EncodingKey, Header};
 use serde::{Deserialize, Serialize};
 
@@ -9,9 +10,10 @@ struct Claims {
 
 pub fn encode(id: &str) -> String {
     let key = b"secret";
+
     let my_claims = Claims {
         sub: id.to_owned(),
-        exp: 100000000,
+        exp: (UTC::now() + Duration::days(1)).timestamp() as usize,
     };
     let token = match jwt_encode(
         &Header::default(),
