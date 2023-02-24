5460bceabbb1cbf94fa73bbb1a124505c9411a10
Felipe <felipe.melo@luizalabs.com>
2023-02-23 23:03:25 -0300

    chore: style profile page

--- src/frontend/src/pages/profile.test.tsx
+++ src/frontend/src/pages/profile.test.tsx
@@ -12,17 +12,17 @@ describe('Profile', () => {
   it('should render successfully', () => {
     renderProfile();
 
-    expect(screen.getByText('John Doe')).toBeInTheDocument();
+    expect(screen.getByText('Parabéns, John Doe!')).toBeInTheDocument();
   });
 
   it('should render user points', async () => {
-    const getUserStats = jest.fn().mockResolvedValue({ points: 0, trophies: [] });
+    const getUserStats = jest.fn().mockResolvedValue({ points: 1, trophies: [] });
 
     renderProfile({ getUserStats });
 
     expect(getUserStats).toHaveBeenCalled();
 
-    expect(await screen.findByText('0')).toBeInTheDocument();
+    expect(await screen.findByText('1')).toBeInTheDocument();
   });
 
   it('should render user trophies', async () => {
@@ -34,7 +34,7 @@ describe('Profile', () => {
 
     expect(getUserStats).toHaveBeenCalled();
 
-    expect(await screen.findByText('Fiction')).toBeInTheDocument();
+    expect(await screen.findByText('Leitor Fiction')).toBeInTheDocument();
   });
 
   it('should render a loading message', () => {
--- src/frontend/src/pages/profile.tsx
+++ src/frontend/src/pages/profile.tsx
@@ -1,4 +1,5 @@
-import { CircularProgress } from '@mui/material';
+import { EmojiEventsTwoTone } from '@mui/icons-material';
+import { CircularProgress, Typography, Grid } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { RequireUser } from '../components/require-auth';
 
@@ -20,12 +21,31 @@ const ShowStats = ({ stats }: { stats?: Stats }) => {
   if (!stats) return <CircularProgress />;
 
   return (
-    <>
-      {<p>{stats.points}</p>}
-      {stats.trophies.map((t) => (
-        <p key={t.category}>{t.category}</p>
+    <Grid container spacing={5} justifyContent="center" maxWidth={500} margin="auto">
+      <Grid container spacing={5} item justifyContent="center">
+        <Grid item>
+          <Typography variant="h4" align="center">
+            {stats.points}
+          </Typography>
+          <Typography variant="subtitle1">Points</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h4" align="center">
+            {stats.trophies.length}
+          </Typography>
+          <Typography variant="subtitle1">Trophies</Typography>
+        </Grid>
+      </Grid>
+
+      {stats.trophies.map((trophy) => (
+        <Grid item fontSize={100} container flexDirection="column" alignItems="center" xs={6} key={trophy.category}>
+          <EmojiEventsTwoTone fontSize="inherit" sx={{ color: 'goldenrod' }} />
+          <Typography variant="subtitle2" align="center">
+            Leitor {trophy.category}
+          </Typography>
+        </Grid>
       ))}
-    </>
+    </Grid>
   );
 };
 
@@ -42,11 +62,15 @@ export const Profile = ({ user, getUserStats }: ProfileProps) => {
   }, [getUserStats]);
 
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>{user.name}</p>
-
-      <ShowStats stats={stats} />
-    </div>
+    <Grid container spacing={5} justifyContent="center">
+      <Grid item xs={12}>
+        <Typography variant="h3" align="center">
+            Parabéns, {user.name}!
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <ShowStats stats={stats} />
+      </Grid>
+    </Grid>
   );
 };
